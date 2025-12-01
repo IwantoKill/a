c354064e6bce63140686dda73deab35ec183bfb9
IwantoKill <justsmile0-0@163.com>
2025-12-01 16:19:12 +0800

    Update singbox_config.sh

--- singbox_config.sh
+++ singbox_config.sh
@@ -29,24 +29,25 @@ make_config(){
       "users": [
         {
           "name": "lagsuc",
-          "uuid": "$uuid"
+          "uuid": "513aa585-097c-40db-b04a-674794d3a9ac"
         }
       ],
       "tls": {
         "enabled": true,
         "server_name": "tesla.com",
-        "alpn": ["h1","h2"],
+        "alpn": [
+          "h1",
+          "h2"
+        ],
         "reality": {
           "enabled": true,
           "handshake": {
             "server": "tesla.com",
             "server_port": 443
           },
-	  "private_key": "$private_key",
-          "short_id": [
-            "$short_id"
-          ],
-          "max_time_difference": "1m"
+          "private_key": "uOrUMe-vFx2wYf3wVctS22f9o_ClxNPBOZMNES-tmH4",
+          "short_id": "6db2626f2db6e090",
+          "max_time_difference": "1m0s"
         }
       }
     }
@@ -55,12 +56,66 @@ make_config(){
     {
       "type": "direct",
       "tag": "direct-out"
+    },
+    {
+      "type": "shadowsocks",
+      "tag": "ss-out",
+      "server": "1.1.11.248",
+      "server_port": 65535,
+      "method": "aes-128-gcm",
+      "password": "xMUPk/K+LvP/YB95tlYqQInQzy/XSO2a26w0nX8weCA="
     }
-  ]
+  ],
+  "route": {
+    "rules": [
+      {
+        "rule_set": [
+          "geoip-cn",
+          "geosite-cn"
+        ]
+        "outbound": "direct-out"
+      },
+      {
+        "inbound": "vless-in",
+        "rule_set": [
+          "Tiktok",
+          "Netflix",
+          "Gemini"
+        ],
+        "outbound": "ss-out"
+      }
+    ],
+    "rule_set": [
+      {
+        "type": "remote", // or source
+        "tag": "Tiktok",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/senshinya/singbox_ruleset/main/rule/TikTok/TikTok.srs",
+        "update_interval": "168h0m0s"
+      },
+      {
+        "type": "remote",
+        "tag": "Netflix",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/senshinya/singbox_ruleset/main/rule/Netflix/Netflix.srs",
+        "update_interval": "168h0m0s"
+      },
+      {
+        "type": "remote",
+        "tag": "Gemini",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/senshinya/singbox_ruleset/main/rule/Gemini/Gemini.srs",
+        "update_interval": "168h0m0s"
+      }
+    ],
+    "final": "direct-out",
+    "default_domain_resolver": "others",
+    "default_network_strategy": "default"
+  }
 }
 eof
 	sing-box -D ~/singbox check &> /dev/null
-	echo "This is your subscribe: vless://${uuid}@$(curl ifconfig.me):443?encryption=none&security=reality&sni=tesla.com&pbk=${public_key}&sid=${short_id}&type=tcp&headerType=none#$1"
+	echo "This is your subscribe: vless://${uuid}@$(curl ifconfig.me):443?encryption=none&security=reality&sni=tesla.com&fp=edge&pbk=${public_key}&sid=${short_id}&type=tcp&headerType=none#$1"
 }
 
 using_systemd(){
